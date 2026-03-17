0897d1baa610a04259b4a3900bb3ee4dd7e58336
Ranit Ghosh <ranitghosh42179@gmail.com>
2026-03-17 21:14:59 +0530

    Add files via upload

---
+++ src/main/java/com/portfolio/chat/bot/controller/Controller.java
@@ -0,0 +1,25 @@
+package com.portfolio.chat.bot.controller;
+
+import com.portfolio.chat.bot.serviceImpl.ServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@AllArgsConstructor
+@RestController
+public class Controller {
+    private final ServiceImpl serviceImpl;
+
+    @GetMapping("/")
+    public String defaultCase(){
+        return "Server is running";
+    }
+    @PostMapping("/gemini/api/v1/ask")
+    public ResponseEntity<String> askQuestion(@RequestBody Map<String, String> payload){
+        String question= payload.get("question");
+        String answer= serviceImpl.getAnswer(question);
+        return ResponseEntity.ok(answer);
+    }
+}
