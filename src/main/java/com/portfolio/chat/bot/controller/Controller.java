03a205fcb5a7ecc3d61ac2fa32bc98c54db354d5
Ranit Ghosh <ranitghosh42179@gmail.com>
2026-03-19 20:30:24 +0530

    added

--- src/main/java/com/portfolio/chat/bot/controller/Controller.java
+++ src/main/java/com/portfolio/chat/bot/controller/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:3001", "https://my-portfolio-8b9j.onrender.com/"})
 @AllArgsConstructor
 @RestController
 public class Controller {
@@ -16,7 +17,6 @@ public class Controller {
     public String defaultCase(){
         return "Server is running";
     }
-    @CrossOrigin(origins = {"http://localhost:3001", "https://my-portfolio-8b9j.onrender.com/"})
     @PostMapping("/gemini/api/v1/ask")
     public ResponseEntity<String> askQuestion(@RequestBody Map<String, String> payload){
         String question= payload.get("question");
